140bbedb887dad9f4747753baf2fffe5b05d0586
Marlon Baptista de Quadros <marlon.quadros@southsystem.com.br>
2021-11-07 21:58:01 -0300

    - Readme

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '2066462883:AAHhFptZs-IkIBl8UO6-X9uwKMRVlU1_pr8')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 WEBHOOK_TOKEN = os.environ.get('WEBHOOK_TOKEN')
 DYNAMO_TABLE_HISTORY_CHAT = os.environ.get('DYNAMO_TABLE_HISTORY_CHAT', 'southon-bot-data-table-dev')
 DYNAMO_TABLE_USER = os.environ.get('DYNAMO_TABLE_USER', 'southon-bot-user-table-dev')
